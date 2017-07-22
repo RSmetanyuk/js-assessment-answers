7440ca3ba043b02aeb33c4aa33ef6b7a316f907a
1 <RSmetanyuk>
2017-07-22 17:20:28 +0300

    regex: endsWithVowel, captureThreeNumbers

--- app/regex.js
+++ app/regex.js
@@ -31,11 +31,32 @@ exports.regexAnswers = {
   },
 
   endsWithVowel: function(str) {
-
+    var vowel = "aeiou";
+    var answer = false;
+    str = str.toLowerCase();
+    for (var i = 0; i < vowel.length; i++) {
+      if (str.endsWith(vowel.charAt(i))) {
+        answer = true;
+        break;
+      }
+    }
+    return answer;
   },
 
   captureThreeNumbers: function(str) {
-
+    var answer = false;
+    for (var i = 0; i < str.length - 2; i++) {
+      if (!isNaN(str.charAt(i)) && !isNaN(str.charAt(i + 1)) && !isNaN(str.charAt(i + 2))) {
+        if (str.charAt(i) - str.charAt(i + 1) === 1 && str.charAt(i + 1) - str.charAt(i + 2) === 1) {
+          answer = str.slice(i, i + 3);
+          break;         
+        } else if (str.charAt(i) - str.charAt(i + 1) === - 1 && str.charAt(i + 1) - str.charAt(i + 2) === - 1) {
+          answer = str.slice(i, i + 3);
+          break; 
+        };
+      };
+    }; 
+    return answer;
   },
 
   matchesPattern: function(str) {
