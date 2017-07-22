9c8856ec423576ec1d347462b45191cf9941b4a2
1 <RSmetanyuk>
2017-07-23 00:06:10 +0300

    regexp: answers reworked with regular expression

--- app/regex.js
+++ app/regex.js
@@ -2,76 +2,26 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.regexAnswers = {
   containsNumber: function(str) {
-    var i = 0;
-    var answer = false;
-    while (i < str.length) {
-      if (isNaN(str.charAt(i))) {
-        i++;
-      } else {
-        answer = true;
-        break;
-      };
-    }
-    return answer;
+    return /\d/.test(str);
   },
 
   containsRepeatingLetter: function(str) {
-    var answer = false;
-    for (var i = 0; i < str.length; i++) {
-      if (!isNaN(str.charAt(i))) continue;
-      for (var j = i + 1; j < str.length; j++) {
-        if(str.charAt(i) === str.charAt(j)) {
-          answer = true;
-          i = str.length
-          break;
-        };
-      };
-    };
-    return answer;
+    return /([a-z])\1+/i.test(str);
   },
 
   endsWithVowel: function(str) {
-    var vowel = "aeiou";
-    var answer = false;
-    str = str.toLowerCase();
-    for (var i = 0; i < vowel.length; i++) {
-      if (str.endsWith(vowel.charAt(i))) {
-        answer = true;
-        break;
-      }
-    }
-    return answer;
+    return /[aeiou]$/i.test(str);
   },
 
   captureThreeNumbers: function(str) {
-    var answer = false;
-    for (var i = 0; i < str.length - 2; i++) {
-      if (!isNaN(str.charAt(i)) && !isNaN(str.charAt(i + 1)) && !isNaN(str.charAt(i + 2))) {
-        if (str.charAt(i) - str.charAt(i + 1) === 1 && str.charAt(i + 1) - str.charAt(i + 2) === 1) {
-          answer = str.slice(i, i + 3);
-          break;         
-        } else if (str.charAt(i) - str.charAt(i + 1) === - 1 && str.charAt(i + 1) - str.charAt(i + 2) === - 1) {
-          answer = str.slice(i, i + 3);
-          break; 
-        };
-      };
-    }; 
-    return answer;
+    return /012|123|234|345|456|567|678|789|987|876|765|654|543|432|321|210/.exec(str)[0];  // NOT PASSED!!!
   },
 
   matchesPattern: function(str) {
-    var answer = false;
-    if (str.length === 12) {
-      if (str.charAt(3) === "-" && str.charAt(7) === "-") {
-        if(!isNaN(str.slice(0, 3)) && !isNaN(str.slice(4, 7)) && !isNaN(str.slice(8))) {
-          answer = true;
-        };
-      };
-    };
-    return answer;
+    return /^(\d){3}-(\d){3}-(\d){4}$/.test(str);
   },
 
   isUSD: function(str) {
-
+    return /^\$(\d){1,3}(,\d\d\d){0,10}(\.\d\d){0,1}$/.test(str);
   }
 };
