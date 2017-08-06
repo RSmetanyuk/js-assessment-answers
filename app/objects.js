90eb0cea0c719a78315d09ea75ef47a8cd3ab091
RSmetanyuk <rsmetanyuk@gmail.com>
2017-08-06 16:30:53 +0300

    objects: alterContext

--- app/objects.js
+++ app/objects.js
@@ -2,9 +2,10 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = { 
   alterContext: function(fn, obj) {		//not passed
-  	name = obj.name;
+  	return fn.call(obj)
+  	/*name = obj.name;
   	greeting = obj.greeting;
-  	fn();
+  	fn();*/
   },
 
   alterObjects: function(constructor, greeting) {
