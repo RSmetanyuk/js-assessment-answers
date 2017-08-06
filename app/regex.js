c4639adf6a1594c58500a1e89088082c0a5b3c2a
RSmetanyuk <rsmetanyuk@gmail.com>
2017-08-06 14:54:02 +0300

    regex: captureThreeNumbers

--- app/regex.js
+++ app/regex.js
@@ -14,8 +14,15 @@ exports.regexAnswers = {
   },
 
   captureThreeNumbers: function(str) {
-    var re = /012|123|234|345|456|567|678|789|987|876|765|654|543|432|321|210/g
-    return re.exec(str);  // NOT PASSED!!!
+    var answer = /012|123|234|345|456|567|678|789|987|876|765|654|543|432|321|210/.exec(str);
+    return answer !== null ? answer[0] : false;
+
+    /*var pos = str.search(/012|123|234|345|456|567|678|789|987|876|765|654|543|432|321|210/);   // alternative variant
+    if (pos >= 0) {
+      return str.substr(pos, 3);
+    } else {
+      return false;
+    }*/
   },
 
   matchesPattern: function(str) {
