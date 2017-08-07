448147307c7b5f236efd9cfb548478de9419c716
1 <RSmetanyuk>
2017-08-07 18:39:18 +0300

    count: count

--- app/count.js
+++ app/count.js
@@ -1,6 +1,12 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
-
-	};
+  count: function (start, end) {
+	if (start <= end) {
+    	console.log(start);
+    	start++;
+    	var myTimer = setTimeout(exports.countAnswers.count, 100, start, end);
+    }
+    return counter = {cancel: function() {clearTimeout(myTimer)}}
+  }
 };
