a928e4d5eaa2f994eaa34586a1f12b02d2d693b5
1 <RSmetanyuk>
2017-08-24 13:39:57 +0300

    modules: createModule

--- app/modules.js
+++ app/modules.js
@@ -2,11 +2,11 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.modulesAnswers = {
   createModule: function(str1, str2) {
-  	this.greeting = str1;
-  	this.name = str2;
-  	sayIt: function() {
-  		return this.greeting + ', ' + this.name;
-  	}
-
+  	var answer = {
+      greeting: str1,
+    	name: str2,
+    	sayIt: function() {return this.greeting + ', ' + this.name}
+    }
+    return answer
   }
 };
